2b23432543bb2b30f88de5ca94807968442f8896
Chunhui Shi <cshi@apache.org>
2019-05-03 02:05:05 -0700

    chkstyle

--- flink-kubernetes/src/main/java/org/apache/flink/kubernetes/kubeclient/fabric8/Fabric8FlinkKubeClient.java
+++ flink-kubernetes/src/main/java/org/apache/flink/kubernetes/kubeclient/fabric8/Fabric8FlinkKubeClient.java
@@ -34,7 +34,6 @@ import org.apache.flink.kubernetes.kubeclient.fabric8.decorators.TaskManagerDeco
 import org.apache.flink.kubernetes.kubeclient.fabric8.decorators.debug.ExternalIPDecorator;
 import org.apache.flink.kubernetes.kubeclient.fabric8.decorators.debug.PodDebugDecorator;
 
-import io.fabric8.kubernetes.api.model.LoadBalancerStatus;
 import io.fabric8.kubernetes.api.model.Pod;
 import io.fabric8.kubernetes.api.model.Service;
 import io.fabric8.kubernetes.client.KubernetesClient;
